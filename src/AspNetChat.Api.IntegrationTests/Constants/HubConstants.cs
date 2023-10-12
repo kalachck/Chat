2bf2429d8e0b8e7f713073202d7c9a6fb6601346
kalachck <73075156+kalachck@users.noreply.github.com.>
2023-10-12 18:23:55 +0200

    add chat tests

---
+++ src/AspNetChat.Api.IntegrationTests/Constants/HubConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace AspNetChat.Api.IntegrationTests.Constants
+{
+    public class HubConstants
+    {
+        public const string ChatHubUrl = "https://localhost:7261/chat";
+
+        public const string JoinChatAsync = "JoinChatAsync";
+
+        public const string LeaveChatAsync = "LeaveChatAsync";
+
+        public const string SendMessageAsync = "SendMessageAsync";
+        
+        public const string UpdateMessageAsync = "UpdateMessageAsync";
+
+        public const string DeleteMessageAsync = "DeleteMessageAsync";
+    }
+}
---
+++ src/AspNetChat.Api.IntegrationTests/HubTests/ChatHubTests.cs
@@ -0,0 +1,227 @@
+﻿using AspNetChat.Api.IntegrationTests.Abstract;
+using AspNetChat.Api.IntegrationTests.Constants;
+using AspNetChat.Models.Message;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.SignalR.Client;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace AspNetChat.Api.IntegrationTests.HubTests
+{
+    public class ChatHubTests : BaseIntegrationTests
+    {
+        private readonly HubConnection _hubConnection;
+
+        public ChatHubTests()
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(HubConstants.ChatHubUrl, options =>
+                {
+                    options.HttpMessageHandlerFactory = _ => _factory.Server.CreateHandler();
+                })
+                .Build();
+        }
+
+        [Fact]
+        public async Task JoinChatAsync_WhenChatExists_ReturnsMessageHistory()
+        {
+            //Arrange
+            var chat = await _dataManager.CreateChatAsync();
+
+            var messages = await _dataManager.CreateMessagesAsync(chat, 3);
+
+            //Act
+            await _hubConnection.StartAsync();
+
+            var messageDtos = await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.JoinChatAsync, chat.ChatName);
+
+            //Assert
+            messageDtos.Should().NotBeNull().And.HaveCount(messages.Count);
+        }
+
+        [Fact]
+        public async Task JoinChatAsync_WhenChatDoesNotExist_ReturnsEmptyMessageHistory()
+        {
+            //Arrange
+            //Act
+            await _hubConnection.StartAsync();
+
+            var messageDtos = await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.JoinChatAsync, "testChat");
+
+            //Assert
+            messageDtos.Should().NotBeNull().And.HaveCount(0);
+        }
+
+        [Fact]
+        public async Task LeaveChatAsync_WhenRequestIsValid_ReturnsSuccessMessage()
+        {
+            //Arrange
+            var chat = await _dataManager.CreateChatAsync();
+
+            var errorMessage = "";
+            var receivedMessageEvent = new ManualResetEvent(false);
+
+            _hubConnection.Closed += async (error) =>
+            {
+                errorMessage = error.Message;
+                receivedMessageEvent.Set();
+            };
+
+            //Act
+            await _hubConnection.StartAsync();
+
+            await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.LeaveChatAsync, chat.ChatName);
+
+            receivedMessageEvent.WaitOne(TimeSpan.FromMilliseconds(100)).Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task SendMessageAsync_WhenRequestIsValid_ReturnsMessageDto()
+        {
+            // Arrange
+            var chat = await _dataManager.CreateChatAsync();
+            var requestModel = _fixture.Build<CreateMessageRequestModel>()
+                .With(x => x.ChatName, chat.ChatName)
+                .With(x => x.UserId, chat.CreatorId)
+                .Create();
+
+            var receivedMessage = "";
+            var receivedMessageEvent = new ManualResetEvent(false);
+
+            _hubConnection.On<string>("SendMessage", message =>
+            {
+                receivedMessage = message;
+                receivedMessageEvent.Set();
+            });
+
+            // Act
+            await _hubConnection.StartAsync();
+
+            await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.JoinChatAsync, "testChat");
+
+            await _hubConnection.InvokeAsync<MessageDto>(HubConstants.SendMessageAsync, requestModel);
+
+            // Assert
+            if (receivedMessageEvent.WaitOne(TimeSpan.FromMilliseconds(100)))
+            {
+                receivedMessage.Should().Be(requestModel.Content);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateMessageAsync_WhenRequestIsValid_ReturnsMessageDto()
+        {
+            // Arrange
+            var chat = await _dataManager.CreateChatAsync();
+            var message = await _dataManager.CreateMessageAsync(chat);
+
+            var requestModel = _fixture.Build<UpdateMessageRequestModel>()
+                .Create();
+
+            var receivedMessageEvent = new ManualResetEvent(false);
+
+            _hubConnection.On<string>("UpdateMessage", msg =>
+            {
+                receivedMessageEvent.Set();
+            });
+
+            // Act
+            await _hubConnection.StartAsync();
+
+            await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.JoinChatAsync, "testChat");
+
+            await _hubConnection.InvokeAsync<MessageDto>(HubConstants.UpdateMessageAsync, message.Id, requestModel);
+
+            // Assert
+            if (receivedMessageEvent.WaitOne(TimeSpan.FromMilliseconds(100)))
+            {
+                message.Should().NotBe(requestModel.Content);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateMessageAsync_WhenMessageDoesNotExist_ReturnsMessageDto()
+        {
+            // Arrange
+            var chat = await _dataManager.CreateChatAsync();
+
+            var requestModel = _fixture.Build<UpdateMessageRequestModel>()
+                .Create();
+
+            var receivedMessage = "";
+            var receivedMessageEvent = new ManualResetEvent(false);
+
+            _hubConnection.On<string>("UpdateMessage", msg =>
+            {
+                receivedMessage = msg;
+
+                receivedMessageEvent.Set();
+            });
+
+            // Act
+            await _hubConnection.StartAsync();
+
+            await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.JoinChatAsync, "testChat");
+
+            await _hubConnection.InvokeAsync<MessageDto>(HubConstants.UpdateMessageAsync, 1, requestModel);
+
+            // Assert
+            receivedMessageEvent.WaitOne(TimeSpan.FromMilliseconds(100)).Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task DeleteMessageAsync_WhenRequestIsValid_ReturnsMessageDto()
+        {
+            // Arrange
+            var chat = await _dataManager.CreateChatAsync();
+            var message = await _dataManager.CreateMessageAsync(chat);
+
+            var receivedResult = false;
+            var receivedMessageEvent = new ManualResetEvent(false);
+
+            _hubConnection.On<bool>("DeleteMessage", result =>
+            {
+                receivedResult = result;
+                receivedMessageEvent.Set();
+            });
+
+            // Act
+            await _hubConnection.StartAsync();
+
+            await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.JoinChatAsync, "testChat");
+
+            await _hubConnection.InvokeAsync<bool>(HubConstants.DeleteMessageAsync, message.Id, chat.ChatName);
+
+            // Assert
+            if (receivedMessageEvent.WaitOne(TimeSpan.FromMilliseconds(100)))
+            {
+                receivedResult.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public async Task DeleteMessageAsync_WhenMessageDoesNotExists_ReturnsMessageDto()
+        {
+            // Arrange
+            var chat = await _dataManager.CreateChatAsync();
+            var message = await _dataManager.CreateMessageAsync(chat);
+
+            var receivedMessageEvent = new ManualResetEvent(false);
+
+            _hubConnection.On<bool>("DeleteMessage", result =>
+            {
+                receivedMessageEvent.Set();
+            });
+
+            // Act
+            await _hubConnection.StartAsync();
+
+            await _hubConnection.InvokeAsync<List<MessageDto>>(HubConstants.JoinChatAsync, "testChat");
+
+            await _hubConnection.InvokeAsync<bool>(HubConstants.DeleteMessageAsync, message.Id, chat.ChatName);
+
+            // Assert
+            receivedMessageEvent.WaitOne(TimeSpan.FromMilliseconds(100)).Should().BeFalse();
+        }
+    }
+}
